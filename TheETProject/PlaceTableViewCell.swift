0e1403e7e4e37df9d5a174dd53f9ff7655aedfce
Belén Molina del Campo <bmolinadelcampo@gmail.com>
2016-05-10 20:20:36 +0100

    Structure navigation and create ViewController classes. Fix #1. Fix #6. Fix #51.

---
+++ TheETProject/PlaceTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PlaceTableViewCell.swift
+//  TheETProject
+//
+//  Created by Belén Molina del Campo on 10/05/2016.
+//  Copyright © 2016 Belén Molina del Campo. All rights reserved.
+//
+
+import UIKit
+
+class PlaceTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ TheETProject/SettingsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SettingsViewController.swift
+//  TheETProject
+//
+//  Created by Belén Molina del Campo on 10/05/2016.
+//  Copyright © 2016 Belén Molina del Campo. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func dismissViewController(sender: AnyObject) {
+        
+        self.dismissViewControllerAnimated(true) { () -> Void in
+            print("updating settings")
+        }
+    }
+    
+}
